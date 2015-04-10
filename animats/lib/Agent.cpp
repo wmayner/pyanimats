c31ded5d93f669c08f1b73bb782646b19f8f786f
Will Mayner <wmayner@gmail.com>
2015-04-10 17:07:17 -0500

    Reorder functions in Agent.cpp

--- animats/lib/Agent.cpp
+++ animats/lib/Agent.cpp
@@ -16,14 +16,42 @@ Agent::Agent() {
     hmmus.clear();
 }
 
-Agent::~Agent() {
-    for (int i = 0; i < hmmus.size(); i++) {
-        delete hmmus[i];
+void Agent::setupEmptyAgent(int nucleotides) {
+    genome.resize(nucleotides);
+    for (int i = 0; i < nucleotides; i++) {
+        genome[i] = 127;
     }
-    if (ancestor != NULL) {
-        ancestor->nrPointingAtMe--;
-        if (ancestor->nrPointingAtMe == 0)
-            delete ancestor;
+    setupPhenotype();
+}
+
+void Agent::setupPhenotype() {
+    if (hmmus.size() != 0) {
+        for (int i = 0; i < hmmus.size(); i++) {
+            delete hmmus[i];
+        }
+    }
+    hmmus.clear();
+    HMMU *hmmu;
+    for (int i = 0; i < genome.size(); i++) {
+        if ((genome[i] == 42) && (genome[(i + 1) %genome.size()] == 213)) {
+            hmmu = new HMMU(genome, i);
+            hmmus.push_back(hmmu);
+        }
+    }
+}
+
+void Agent::resetBrain(void) {
+    for (int i = 0; i < NUM_NODES; i++)
+        states[i] = 0;
+}
+
+void Agent::updateStates(void) {
+    for (int i = 0; i < hmmus.size(); i++) {
+        hmmus[i]->update(&states[0], &newStates[0]);
+    }
+    for (int i = 0; i < NUM_NODES; i++) {
+        states[i] = newStates[i];
+        newStates[i] = 0;
     }
 }
 
@@ -37,14 +65,6 @@ void Agent::injectStartCodons() {
         for (int k = 2; k < 20; k++)
             genome[j + k] = rand() & 255;
     }
-}
-
-void Agent::setupEmptyAgent(int nucleotides) {
-    genome.resize(nucleotides);
-    for (int i = 0; i < nucleotides; i++) {
-        genome[i] = 127;
-    }
-    setupPhenotype();
 }
 
 void Agent::inherit(Agent *parent, double mutationRate, int generation) {
@@ -85,33 +105,14 @@ void Agent::inherit(Agent *parent, double mutationRate, int generation) {
     fitness = 0.0;
 }
 
-void Agent::setupPhenotype() {
-    if (hmmus.size() != 0) {
-        for (int i = 0; i < hmmus.size(); i++) {
-            delete hmmus[i];
-        }
-    }
-    hmmus.clear();
-    HMMU *hmmu;
-    for (int i = 0; i < genome.size(); i++) {
-        if ((genome[i] == 42) && (genome[(i + 1) %genome.size()] == 213)) {
-            hmmu = new HMMU(genome, i);
-            hmmus.push_back(hmmu);
-        }
-    }
-}
-
-void Agent::resetBrain(void) {
-    for (int i = 0; i < NUM_NODES; i++)
-        states[i] = 0;
-}
-
-void Agent::updateStates(void) {
+Agent::~Agent() {
     for (int i = 0; i < hmmus.size(); i++) {
-        hmmus[i]->update(&states[0], &newStates[0]);
+        delete hmmus[i];
     }
-    for (int i = 0; i < NUM_NODES; i++) {
-        states[i] = newStates[i];
-        newStates[i] = 0;
+    if (ancestor != NULL) {
+        ancestor->nrPointingAtMe--;
+        if (ancestor->nrPointingAtMe == 0)
+            delete ancestor;
     }
 }
+
