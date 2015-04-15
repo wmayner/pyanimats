037cea8e66f856ae5e366bed0047af1c02d23b1c
Will Mayner <wmayner@gmail.com>
2015-04-15 15:54:53 -0500

    Add constants.hpp

---
+++ animats/lib/constants.hpp
@@ -0,0 +1,14 @@
+// constants.h
+
+#ifndef SRC_CONSTANTS_H_
+#define SRC_CONSTANTS_H_
+
+// Agent parameters
+#define NUM_NODES 8
+#define DETERMINISTIC true
+
+// World parameters
+#define WORLD_HEIGHT 36
+#define WORLD_WIDTH 16
+
+#endif  // SRC_CONSTANTS_H_
