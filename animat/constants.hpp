59eb2060ea029397717e0f1f2bba9a303cc78b49
Will Mayner <wmayner@gmail.com>
2015-05-18 16:55:14 -0500

    Note which animat params must be powers of 2

--- animat/constants.hpp
+++ animat/constants.hpp
@@ -4,6 +4,7 @@
 #define SRC_CONSTANTS_H_
 
 // Agent parameters
+// ----------------
 #define NUM_NODES 8
 #define NUM_STATES (1 << NUM_NODES)
 #define NUM_SENSORS 2
@@ -11,10 +12,14 @@
 #define DETERMINISTIC true
 
 // World parameters
+// ----------------
 #define WORLD_HEIGHT 36
+// IMPORTANT: Must be a power of 2
 #define WORLD_WIDTH 16
 
 // Evolution parameters
+// ---------------------
+// IMPORTANT: These must be a power of 2
 // Minimum length of a duplicated/deleted genome section
 #define MIN_DUP_DEL_LENGTH 15
 // Maximum length of a duplicated/deleted genome section
