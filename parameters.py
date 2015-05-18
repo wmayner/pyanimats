5c6d00befd5c82d220d9dad5f78105eec87566c2
Will Mayner <wmayner@gmail.com>
2015-05-18 15:58:55 -0500

    Get parameters from animat module, add param print

--- parameters.py
+++ parameters.py
@@ -3,9 +3,11 @@
 # parameters.py
 
 import sys
+import animat
+from pprint import pprint
 
 
-param_dict = {
+params = {
     # Simulation parameters
     'NGEN': 60000,
     'POPSIZE': 100,
@@ -20,13 +22,38 @@ param_dict = {
     # Evolution parameters
     'MUTATION_PROB': 0.005,
     'FITNESS_BASE': 1.02,
-    'INIT_GENOME': [127] * 5000,
     'DUPLICATION_PROB': 0.05,
     'DELETION_PROB': 0.02,
     'MAX_GENOME_LENGTH': 10000,
     'MIN_GENOME_LENGTH': 1000,
     'MIN_DUP_DEL_WIDTH': 15,
     'MAX_DUP_DEL_WIDTH': 511,
+    'INIT_GENOME': [127] * 5000,
+    # Game parameters
+    'WORLD_WIDTH': animat.WORLD_WIDTH,
+    'WORLD_HEIGHT': animat.WORLD_HEIGHT,
+    'NUM_NODES': animat.NUM_NODES,
+    'NUM_SENSORS': animat.NUM_SENSORS,
+    'NUM_MOTORS': animat.NUM_MOTORS,
+    'DETERMINISTIC': animat.DETERMINISTIC,
 }
+params['NUM_TRIALS'] = len(params['TASKS']) * 2 * params['WORLD_WIDTH']
 
-sys.modules[__name__].__dict__.update(param_dict)
+
+# Update module namespace with parameters
+sys.modules[__name__].__dict__.update(params)
+
+
+def printable_params():
+    d = params.copy()
+    # Don't print initial genome
+    del d['INIT_GENOME']
+    return d
+
+
+def print_parameters():
+    print(''.center(50, '-'))
+    print('PARAMETERS:')
+    print(''.center(50, '-'))
+    pprint(printable_params())
+    print(''.center(50, '-'))
