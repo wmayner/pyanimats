d0150210e1b5cfa921ab5f3022f4036a8e74564d
Will Mayner <wmayner@gmail.com>
2015-06-11 12:19:55 -0500

    Implement mutual information fitness function

--- fitness_functions.py
+++ fitness_functions.py
@@ -6,6 +6,10 @@
 Fitness functions for driving animat evolution.
 """
 
+import math
+import numpy as np
+from sklearn.metrics import mutual_info_score
+
 from parameters import params
 
 
@@ -30,9 +34,10 @@ def print_functions():
 
 @register
 def natural(ind):
-    """Animats are evaluated based on the number of trials they successfully
-    complete. Each additional correct trial is weighted exponentially higher
-    than the last in order to keep selection pressure more even.
+    """Animats are evaluated based on the number of game trials they
+    successfully complete. Each additional correct trial is weighted
+    exponentially higher than the last in order to keep selection pressure more
+    even.
 
     This is accomplished by using the ``NATURAL_FITNESS_BASE`` parameter as the
     base and the number of correct trials as the exponent."""
@@ -41,7 +46,27 @@ def natural(ind):
 
 
 @register
-def mutual_information(ind):
+def mi(ind):
     # TODO implement mutual information
-    """MUTUAL INFORMATION DESCRIPTION"""
-    pass
+    """Animats are evaluated based on the mutual information between the
+    sensors and motors."""
+    # Play the game and get the state transitions for each trial.
+    game = ind.play_game()
+    # The contingency matrix has a row for every sensors state and a column for
+    # every motor state.
+    c = np.zeros([2**params.NUM_SENSORS, 2**params.NUM_MOTORS])
+    # Count!
+    for trial in game:
+        for state in trial:
+            # Get contingency matrix row and column indices from sensor and
+            # motor states and increment the count.
+            i, j = 0, 0
+            for node in state[:params.NUM_SENSORS]:
+                i = (i << 1) | node
+            for node in state[-params.NUM_MOTORS:]:
+                j = (j << 1) | node
+            c[i, j] += 1
+    # Calculate mutual information in nats.
+    mi_nats = mutual_info_score(None, None, contingency=c)
+    # Convert from nats to bits and return as a tuple for DEAP.
+    return (mi_nats / math.log(2),)
