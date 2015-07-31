f37f4ad5ca87a25e75ab977341bbe5a024c87594
Will Mayner <wmayner@gmail.com>
2015-07-31 16:16:42 -0500

    Flesh-out unique_rows tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -5,8 +5,8 @@
 import pytest
 import numpy as np
 
-import fitness_functions as ff
 from conftest import p
+from utils import unique_rows
 
 
 @pytest.fixture()
@@ -23,8 +23,22 @@ def a():
     ])
 
 
-def test_unique_rows(a):
-    result = ff.unique_rows(a)
+def test_unique_rows_empty():
+    result = unique_rows(np.array([[]]))
+    answer = np.array([[]])
+    p(result, answer)
+    assert np.array_equal(result, answer)
+
+
+def test_unique_rows_one_row():
+    result = unique_rows(np.array([[0]]))
+    answer = np.array([[0]])
+    p(result, answer)
+    assert np.array_equal(result, answer)
+
+
+def test_unique_rows_no_secondary(a):
+    result = unique_rows(a)
     answer = np.array([
         [1, 0, 0, 0, 0],
         [1, 1, 0, 0, 0],
@@ -37,7 +51,7 @@ def test_unique_rows(a):
 
 # TODO implement unique_rows tests
 def test_unique_rows_subset_columns(a):
-    result = ff.unique_rows(a, upto=[0, 2, 3])
+    result = unique_rows(a, upto=[0, 2, 3])
     answer = np.array([
         [1, 1, 0, 0, 0],
         [0, 0, 1, 1, 1],
@@ -47,7 +61,7 @@ def test_unique_rows_subset_columns(a):
 
 
 def test_unique_rows_one_column(a):
-    result = ff.unique_rows(a, upto=[0])
+    result = unique_rows(a, upto=[0])
     answer = np.array([
         [0, 0, 1, 1, 1],
         [1, 1, 0, 0, 0],
@@ -58,14 +72,47 @@ def test_unique_rows_one_column(a):
 
 # TODO implement unique_rows tests
 def test_unique_rows_with_counts(a):
-    result = ff.unique_rows(a, counts=True)
-    answer = [
-        (np.array([1, 1, 0, 0, 0]), 3),
-        (np.array([1, 0, 0, 0, 1]), 2),
-        (np.array([0, 0, 1, 1, 1]), 2),
-        (np.array([1, 0, 0, 0, 0]), 1),
-    ]
+    result = unique_rows(a, counts=True)
+    answer = (np.array([[1, 1, 0, 0, 0],
+                        [0, 0, 1, 1, 1],
+                        [1, 0, 0, 0, 1],
+                        [1, 0, 0, 0, 0]]),
+              np.array([3, 2, 2, 1]))
     p(result, answer)
     for r, a in zip(result, answer):
-        assert np.array_equal(r[0], a[0])
-        assert r[1] == a[1]
+        assert np.array_equal(r, a)
+
+
+def test_unique_rows_with_indices(a):
+    result = unique_rows(a, indices=True)
+    answer = (np.array([[1, 0, 0, 0, 0],
+                        [1, 1, 0, 0, 0],
+                        [1, 0, 0, 0, 1],
+                        [0, 0, 1, 1, 1]]),
+              np.array([3, 0, 2, 1, 1, 2, 1, 3]))
+    p(result, answer)
+    for r, a in zip(result, answer):
+        assert np.array_equal(r, a)
+
+
+def test_unique_rows_with_counts_and_indices(a):
+    result = unique_rows(a, indices=True, counts=True)
+    answer = (np.array([[1, 1, 0, 0, 0],
+                        [0, 0, 1, 1, 1],
+                        [1, 0, 0, 0, 1],
+                        [1, 0, 0, 0, 0]]),
+              np.array([3, 0, 2, 1, 1, 2, 1, 3]),
+              np.array([3, 2, 2, 1]))
+    p(result, answer)
+    for r, a in zip(result, answer):
+        assert np.array_equal(r, a)
+
+
+def test_unique_rows_with_counts_and_indices(a):
+    result = unique_rows(a, sort=True)
+    answer = np.array([[1, 1, 0, 0, 0],
+                       [0, 0, 1, 1, 1],
+                       [1, 0, 0, 0, 1],
+                       [1, 0, 0, 0, 0]])
+    p(result, answer)
+    assert np.array_equal(result, answer)
