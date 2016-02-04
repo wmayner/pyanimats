8d549612c50c05a8b1e3a9caa6dfb152de7ec891
Will Mayner <wmayner@gmail.com>
2016-02-04 15:35:08 -0600

    Import newly exposed C++ constants to Python

--- constants.py
+++ constants.py
@@ -9,16 +9,16 @@ Populated by :mod:`configure`.
 """
 
 import math
-import animat
 
+import animat
+from animat import MIN_BODY_LENGTH
 
 NAT_TO_BIT_CONVERSION_FACTOR = 1 / math.log(2)
-START_CODON = [42, 213]
+START_CODON = [animat.START_CODON_NUCLEOTIDE_ONE,
+               animat.START_CODON_NUCLEOTIDE_TWO]
 DEFAULT_INIT_GENOME = [127] * 5000
 INIT_GENOME = [127] * 5000
-HIT_TYPE = {
-    animat.CORRECT_CATCH: 'CORRECT_CATCH',
-    animat.WRONG_CATCH: 'WRONG_CATCH',
-    animat.CORRECT_AVOID: 'CORRECT_AVOID',
-    animat.WRONG_AVOID: 'WRONG_AVOID',
-}
+HIT_TYPE = {animat.CORRECT_CATCH: 'CORRECT_CATCH',
+            animat.WRONG_CATCH: 'WRONG_CATCH',
+            animat.CORRECT_AVOID: 'CORRECT_AVOID',
+            animat.WRONG_AVOID: 'WRONG_AVOID'}
