9c898ed259d250bc3b04f53b29c52b202ac4bcc5
Will Mayner <wmayner@gmail.com>
2016-02-04 15:38:43 -0600

    Derive parameters in `Experiment`
    - Compute secondary, derived parameters on initialization of the
      `Experiment` object
    - Add more documentation to `Experiment`

--- experiment.py
+++ experiment.py
@@ -2,48 +2,188 @@
 # -*- coding: utf-8 -*-
 # experiment.py
 
+import os
+import pickle
 import pprint
+from copy import copy
 
+import pyphi
 import yaml
 from munch import Munch
+
+import constants
 
 
 class Experiment(Munch):
 
-    """A parameter set specifying an evolutionary simulation.
+    """Parameters specifying an evolutionary simulation.
 
-    Parameters can be accessed as attributes, i.e. with dot notation.
+    These can be accessed as attributes, i.e. with dot notation.
+
+    **Note on magic:** there are several parameters that are derived from those
+    provided upon initialization; these can also be accessed directly as
+    attributes on this object, though they're stored under the ``_derived`` key
+    and are not printed. See ``experiment._derived.keys()`` for a list of
+    these (the key ``_derived`` will be overwritten upon initialization; don't
+    use it).
 
     Keyword Args:
         filepath (string): A file path pointing to a YAML file containing
             experiment parameters.
-        dictionary (string): A dictionary of experiment parameters. Values in
+        override (string): A dictionary of experiment parameters. Values in
             this dictionary overwrite values from the file.
 
     Example:
         >>> e = Experiment('experiments/example.yml')
         >>> e.num_sensors
         3
+        >>> # This is a derived parameter that was not present in the file:
+        >>> e.sensor_indices
+        (0, 1, 2)
     """
 
-    def __init__(self, filepath=None, dictionary=None):
+    def __init__(self, filepath=None, override=None):
+        dictionary = {}
         # Store the filepath in case the user needs to remember it later.
         self.filepath = filepath
         # Load the given YAML file if provided.
         if filepath is not None:
             with open(filepath) as f:
-                self.update(yaml.load(f))
+                dictionary.update(yaml.load(f))
         # Update from the dictionary if provided.
-        if dictionary is not None:
-            self.update(dictionary)
+        if override is not None:
+            dictionary.update(override)
+        # Derive parameters from the user-set ones.
+        dictionary['_derived'] = _derive_params(dictionary)
+        # Put everything in the Munch.
+        self.update(dictionary)
+
+    def __getattr__(self, k):
+        """Fall back on derived parameters if ``k`` is not an attribute."""
+        try:
+            object.__getattribute__(self, k)
+        except AttributeError:
+            try:
+                return self[k]
+            except KeyError:
+                return getattr(self._derived, k)
 
     def __repr__(self):
+        """Return a readable representation of the experiment.
+
+        This does not include derived parameters.
+        """
+        # Exclude derived parameters.
+        printable = {k: v for k, v in self.items() if k != '_derived'}
+        # Format with pretty print, and indent.
         return ('Experiment({\n ' +
-                indent(pprint.pformat(dict(self), indent=1)[1:-1], amount=1) +
+                _indent(pprint.pformat(printable, indent=1)[1:-1], amount=1) +
                 '\n})')
 
 
-def indent(lines, amount=2, chr=' '):
+def _derive_params(d):
+    """Derive various secondary parameters from the given dictionary."""
+    num_nodes = d['num_sensors'] + d['num_hidden'] + d['num_motors']
+    # Load initial genome if provided.
+    if d['init_genome_path']:
+        path = os.path.join(d['init_genome'], 'lineages.pkl')
+        with open(path, 'rb') as f:
+            lineages = pickle.load(f)
+            # Use the genome of the best individual of the most recent
+            # generation.
+            init_genome = lineages[0][0].genome
+    # If no initial genome was given, then the we inject start codons.
+    elif d['init_start_codons']:
+        init_genome = copy(constants.DEFAULT_INIT_GENOME)
+        gap = len(init_genome) // d['init_start_codons']
+        for i in range(d['init_start_codons']):
+            init_genome[(i * gap):(i * gap + 1)] = constants.START_CODON
+    # If no fitness transform was given, use the defaults.
+    if 'fitness_transform' not in d:
+        # The fitness scale of mutual information depends on the number of
+        # sensors/motors.
+        fitness_transform = constants.FITNESS_TRANSFORMS[d['fitness_function']]
+        if d['fitness_function'] in ['mi', 'mi_wvn']:
+            fitness_transform = {'scale': 64 / min(d['num_sensors'],
+                                                   d['num_motors']),
+                                 'add': 64}
+    sensor_indices = tuple(range(d['num_sensors']))
+    hidden_indices = tuple(range(
+        d['num_sensors'], d['num_sensors'] + d['num_hidden']))
+    motor_indices = tuple(range(
+        d['num_sensors'] + d['num_hidden'],
+        d['num_sensors'] + d['num_hidden'] + d['num_motors']))
+    num_sensor_states = 2**d['num_sensors']
+    num_hidden_states = 2**d['num_hidden']
+    num_motor_states = 2**d['num_motors']
+    sensor_motor_states = [
+        ((i, j), (_bitlist(i, d['num_sensors']) +
+                  _bitlist(j, d['num_motors'])))
+        for i in range(num_sensor_states) for j in range(num_motor_states)
+    ]
+    # Get sensor locations (mapping them to the sensor index).
+    if d['num_sensors'] < constants.MIN_BODY_LENGTH:
+        gap = constants.MIN_BODY_LENGTH - d['num_sensors']
+        gap_start = constants.MIN_BODY_LENGTH // 2
+        sensor_locations = (tuple(range(gap_start)) +
+                            tuple(range(gap_start + gap,
+                                        constants.MIN_BODY_LENGTH)))
+    else:
+        sensor_locations = list(range(d['num_sensors']))
+    # Fill and return the dictionary.
+    return Munch({
+        'num_nodes': num_nodes,
+        'init_genome': init_genome,
+        'fitness_transform': fitness_transform,
+        'num_trials': len(d['task']) * 2 * d['world_width'],
+        'hit_multipliers': [condition[0] for condition in d['task']],
+        # Convert task-strings into integers. Note that in the C++
+        # implementation, the world is mirrored; hence the reversal of the
+        # string.
+        'block_patterns': [int(condition[1][::-1], 2)
+                           for condition in d['task']],
+        # Number of trials is given by
+        #   (number of tasks * two directions *
+        #    number of initial positions for the animat)
+        'sensor_indices': sensor_indices,
+        'hidden_indices': hidden_indices,
+        'motor_indices': motor_indices,
+        'sensor_hidden_indices': sensor_indices + hidden_indices,
+        'hidden_motor_indices': hidden_indices + motor_indices,
+        'sensor_motor_indices': sensor_indices + motor_indices,
+        # Get their power sets.
+        'hidden_powerset': tuple(pyphi.utils.powerset(hidden_indices)),
+        'sensors_and_hidden_powerset': tuple(
+            pyphi.utils.powerset(sensor_indices + hidden_indices)),
+        'hidden_and_motor_powerset': tuple(
+            pyphi.utils.powerset(hidden_indices + motor_indices)),
+        # Get information about possible animat states.
+        'num_sensor_states': num_sensor_states,
+        'num_hidden_states': num_hidden_states,
+        'num_motor_states': num_motor_states,
+        'num_possible_states': 2**num_nodes,
+        'possible_states': [pyphi.convert.loli_index2state(i, num_nodes)
+                            for i in range(2**num_nodes)],
+        'sensor_motor_state': sensor_motor_states,
+        'sensor_locations': sensor_locations,
+    })
+
+
+def _bitlist(i, padlength):
+    """Return a list of the bits of an integer, padded up to ``padlength``.
+
+    Args:
+        i (int): A decimal number.
+        padlength (int): The desired length of the binary representation,
+            padded with leading zeros.
+
+    Returns:
+        bitlist (list(int)): The list of bits.
+    """
+    return list(map(int, bin(i)[2:].zfill(padlength)))
+
+
+def _indent(lines, amount=2, chr=' '):
     """Indent a string.
 
     Prepends whitespace to every line in the passed string. (Lines are
