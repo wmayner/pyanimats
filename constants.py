4cdf0ef2203e479ae0b30cb38d5bf9fc0edb2e64
Will Mayner <wmayner@gmail.com>
2016-02-04 15:35:44 -0600

    Move default fitness transforms
    - Move default fitness transforms from `configure.py` to `constants.py`
    - Remove sensor-dependent scaling of mutual information; this will be
      done in `experiment`

--- constants.py
+++ constants.py
@@ -22,3 +22,19 @@ HIT_TYPE = {animat.CORRECT_CATCH: 'CORRECT_CATCH',
             animat.WRONG_CATCH: 'WRONG_CATCH',
             animat.CORRECT_AVOID: 'CORRECT_AVOID',
             animat.WRONG_AVOID: 'WRONG_AVOID'}
+# Scale raw fitness values so they're in the range 64–128
+# before using them as an exponent (this depends on which fitness function
+# is used).
+FITNESS_TRANSFORMS = {
+    'nat':       {'scale': 1,      'add': 0},
+    'mi':        {'scale': 64,     'add': 64},
+    'mi_wvn':    {'scale': 64,     'add': 64},
+    'ex':        {'scale': 64 / 4, 'add': 64},
+    'ex_wvn':    {'scale': 64 / 1, 'add': 64},
+    'sp':        {'scale': 64 / 4, 'add': 64},
+    'sp_wvn':    {'scale': 64 / 4, 'add': 64},
+    'bp':        {'scale': 64 / 4, 'add': 64},
+    'bp_wvn':    {'scale': 63 / 4, 'add': 64},
+    'state_wvn': {'scale': 64 / 8, 'add': 64},
+    'mat':       {'scale': 64 / 1, 'add': 64},
+}
