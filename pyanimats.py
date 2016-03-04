29f98a7a5abe733d06ae3a72522017ab2b1aedf7
Will Mayner <wmayner@gmail.com>
2016-03-04 15:37:36 -0600

    Refactor CLI
    - Allow `resume` to take options; passing `-n 0` will generate output
      from checkpoint immediately
    - Split options into sections
    - Shorten argument names
    - Document arguments

--- pyanimats.py
+++ pyanimats.py
@@ -8,36 +8,62 @@ PyAnimats
 Evolve animats.
 
 Usage:
-    pyanimats.py run <path/to/experiment.yml> <path/to/output_file> [options]
-    pyanimats.py resume <path/to/checkpoint.pkl> <path/to/output_file> [options]
+    pyanimats.py run <experiment.yml> <output_file> [options]
+    pyanimats.py resume <checkpoint.pkl> <output_file> [options]
     pyanimats.py -h | --help
     pyanimats.py -v | --version
     pyanimats.py --list
 
+Arguments:
+    run <experiment.yml>     Run an experiment.
+    resume <checkpoint.pkl>  Resume from a checkpoint file.
+    <output_file>            File where the output should be stored.
+
 Command-line options override the parameters given in the experiment file.
 
-Options:
+If resuming from a checkpoint, only simulation and data options (listed below)
+have an effect.
+
+To generate output from a checkpoint immediately, without simulating any more
+generations, resume from the checkpoint and pass the option `-n 0` (or any
+value lower than the number of generations already simulated).
+
+General options:
     --list                     List available fitness functions
     -h --help                  Show this
     -v --version               Show version
     -F --force                 Overwrite the output file.
-    -r --rng-seed=INT          Random number generator seed
+    -P --profile=PATH          Profile performance and store results at PATH
+
+Simulation options:
     -c --checkpoint=INT        Checkpoint interval (minutes)
     -C --checkpoint-file=PATH  Save to this checkpoint file (defaults to
                                `checkpoint.pkl` in the output directory)
     -s --status-interval=INT   Status-printing interval (generations)
-    -b --logbook-interval=INT  Logbook recording interval (generations)
+
+Data options:
     -o --output-samples=INT    Number of animats to sample from evolution
+    -b --logbook-interval=INT  Logbook recording interval (generations)
+    -a --all-lineages          Save lineages of entire final population
+
+Evolution options:
+    -r --rng-seed=INT          Random number generator seed
     -f --fitness=FUNC          Fitness function
-    -n --num-gen=NGEN          Number of generations to simulate
+    -n INT --num-gen=NGEN      Number of generations to simulate
     -p --pop-size=INT          Population size
     -g --init-genome=PATH      Path to a lineage file for an intial genome
     -j --jumpstart=INT         Begin with this many start codons
+
+Animat options:
     -e --num-sensors=INT       The number of sensors in an animat
     -i --num-hidden=INT        The number of hidden units in an animat
     -t --num-motors=INT        The number of motors in an animat
+
+Environment options:
     -W --world-width=INT       The width of the animats' environment
     -H --world-height=INT      The height of the animats' environment
+
+Genetic options:
     -m --mut-prob=FLOAT        Point mutation probability
     -U --dup-prob=FLOAT        Duplication probability
     -E --del-prob=FLOAT        Deletion probability
@@ -45,8 +71,6 @@ Options:
     -D --max-dup-del=INT       Maximum length of duplicated/deleted genome part
     -l --min-length=INT        Minimum genome length
     -L --max-length=INT        Maximum genome length
-    -a --all-lineages          Save lineages of entire final population
-    -P --profile=PATH          Profile performance and store results at PATH
 """
 
 import cProfile
@@ -64,7 +88,7 @@ from experiment import Experiment
 # Map CLI options to experiment parameter names and types.
 cli_opt_to_param = {
     '--rng-seed':         ('rng_seed', int),
-    '--checkpoint':       ('checkpoint_interval', int),
+    '--checkpoint':       ('checkpoint_interval', float),
     '--status-interval':  ('status_interval', int),
     '--logbook-interval': ('logbook_interval', int),
     '--output-samples':   ('output_samples', int),
@@ -96,7 +120,7 @@ def main(arguments):
         return 0
 
     # Final output will be written here.
-    OUTPUT_FILE = arguments['<path/to/output_file>']
+    OUTPUT_FILE = arguments['<output_file>']
     # Don't overwrite the output file or without permission.
     if not arguments['--force'] and os.path.exists(OUTPUT_FILE):
         raise FileExistsError(
@@ -107,30 +131,35 @@ def main(arguments):
         utils.ensure_exists(os.path.dirname(OUTPUT_FILE))
     # Checkpoints will be written here.
     CHECKPOINT_FILE = (arguments['--checkpoint-file'] or
-                       arguments['<path/to/checkpoint.pkl>'] or
+                       arguments['<checkpoint.pkl>'] or
                        os.path.join(os.path.dirname(OUTPUT_FILE),
                                     'checkpoint.pkl'))
     # Ensure checkpoint directory exists.
     if os.path.dirname(CHECKPOINT_FILE):
         utils.ensure_exists(os.path.dirname(CHECKPOINT_FILE))
 
+    # Parse the CLI options.
+    cli_options = {param[0]: param[1](arguments[opt])
+                   for opt, param in cli_opt_to_param.items()
+                   if arguments[opt] is not None}
+
     # Either load from a checkpoint or start a new evolution.
     if arguments['resume']:
         # Load the checkpoint.
-        print('Loading checkpoint from `{}`... '.format(CHECKPOINT_FILE),
+        print('Loading checkpoint from `{}`... '
+              ''.format(arguments['<checkpoint.pkl>']),
               end='', flush=True)
-        with open(arguments['<path/to/checkpoint.pkl>'], 'rb') as f:
+        with open(arguments['<checkpoint.pkl>'], 'rb') as f:
             evolution = pickle.load(f)
+        # Update the evolution experiment file with simulation parameters.
+        # TODO split into evolution params and animat params
+        evolution.experiment.update(cli_options)
         print('done.')
         print('Resuming evolution from generation '
               '{}...\n'.format(evolution.generation))
     else:
-        # Load the experiment object, overriding if necessary with CLI options.
-        cli_overrides = {param[0]: param[1](arguments[opt])
-                         for opt, param in cli_opt_to_param.items()
-                         if arguments[opt] is not None}
-        experiment = Experiment(filepath=arguments['<path/to/experiment.yml>'],
-                                override=cli_overrides)
+        experiment = Experiment(filepath=arguments['<experiment.yml>'],
+                                override=cli_options)
         # Initialize the simulation.
         evolution = Evolution(experiment)
         print('Simulating {} generations...'.format(experiment.ngen))
@@ -143,7 +172,7 @@ def main(arguments):
         pr.enable()
 
     # Run it!
-    evolution.run(CHECKPOINT_FILE)
+    evolution.run(CHECKPOINT_FILE, ngen=cli_options.get('ngen'))
 
     if PROFILE_FILEPATH:
         pr.disable()
@@ -152,7 +181,7 @@ def main(arguments):
         pr.dump_stats(PROFILE_FILEPATH)
         print('done.')
 
-    print('\nSimulated {} generations in {}.'.format(
+    print('Simulated {} generations in {}.'.format(
         evolution.generation, utils.compress(evolution.elapsed)))
     print('\nSaving output to `{}`... '.format(OUTPUT_FILE),
           end='', flush=True)
