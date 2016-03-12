bf1fce6c9bd37bf92682e13ed9a2bed239e92230
Will Mayner <wmayner@gmail.com>
2016-03-11 18:05:35 -0600

    Implement TinyDB database utils

---
+++ pyanimats/db.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# db.py
+
+"""Methods for interacting with a TinyDB database of PyAnimats output."""
+
+import json
+import os
+from glob import glob
+
+import data
+import tinydb
+from tinydb import TinyDB
+
+
+# TODO figure out `.all`?
+class AnimatTable(tinydb.database.Table):
+
+    """Convert PyAnimats JSON output back into PyAnimats objects."""
+
+    def search(self, *args, **kwargs):
+        return list(map(data.load_dict, super().search(*args, **kwargs)))
+
+    def get(self, *args, **kwargs):
+        return data.load_dict(super().get(*args, **kwargs))
+
+
+# Use AnimatTable as the default table for TinyDB databases.
+TinyDB.table_class = AnimatTable
+
+
+def insert_all(db, directory, pattern='output.json'):
+    """Recursively insert files matching ``pattern``."""
+    paths = glob(os.path.join(directory, '**', pattern))
+    paths += glob(os.path.join(directory, '**', pattern))
+    data = []
+    for path in paths:
+        with open(path, 'r') as f:
+            data.append(json.load(f))
+    return db.insert_multiple(data)
