1495791ae16e0fb1e47c4ed0aba645746db13a5f
Will Mayner <wmayner@gmail.com>
2016-03-16 16:27:56 -0500

    Include `None` ancestor in `Animat.lineage`

--- pyanimats/animat.py
+++ pyanimats/animat.py
@@ -220,6 +220,7 @@ class Animat:
             if ancestor.gen % step == 0:
                 yield ancestor
             ancestor = ancestor.parent
+        yield ancestor
 
     def mutate(self):
         """Mutate the animat's genome in-place."""
