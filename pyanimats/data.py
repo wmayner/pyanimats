8b1d3f6e70421a7366005e8de6f7205b1f99bbfc
Will Mayner <wmayner@gmail.com>
2016-04-14 15:01:29 -0500

    Implement `data.load_all` function

--- pyanimats/data.py
+++ pyanimats/data.py
@@ -4,8 +4,10 @@
 
 """Data management."""
 
+import os
 import gzip
 import json
+from glob import glob
 
 from . import evolve
 
@@ -17,3 +19,21 @@ def load(filepath, gzipped=False):
         _open = open
     with _open(filepath, 'r') as f:
         return evolve.from_json(json.load(f))
+
+
+def load_all(directory, pattern=os.path.join('**', 'output.json'),
+             convert=True):
+    """Recursively load files matching ``pattern``."""
+    data = []
+    paths = glob(os.path.join(directory, pattern))
+    for path in paths:
+        print('Loading {}...'.format(path))
+        try:
+            with open(path, 'r') as f:
+                data.append(json.load(f))
+        except Exception as e:
+            print('  Error: {}'.format(e))
+            print('  Could not load file; skipping...')
+    if convert:
+        data = list(map(evolve.from_json, data))
+    return data
