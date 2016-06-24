563ff32395238f8e3e168ae2e576d5b58eb5b1ad
Will Mayner <wmayner@gmail.com>
2016-06-24 15:57:56 -0500

    Use default destructor for abstract gate

--- pyanimats/c_animat/Gate.hpp
+++ pyanimats/c_animat/Gate.hpp
@@ -10,14 +10,12 @@ using std::vector;
 // Abstract base class for different types of gates
 class Gate {
  public:
-    virtual ~Gate() = 0;
+    virtual ~Gate() = default;
 
     vector<unsigned char> inputs, outputs;
 
     virtual void update(vector<unsigned char> &currentStates,
             vector<unsigned char> &nextStates) = 0;
 };
-
-inline Gate::~Gate() {}
 
 #endif  // ANIMAT_GATE_H_
