c8864d434609944fcb8b60b2fa20f20bdce3efda
Will Mayner <wmayner@gmail.com>
2016-07-11 17:40:02 -0500

    Revert to just one update step per world tick

--- pyanimats/c_animat/Game.cpp
+++ pyanimats/c_animat/Game.cpp
@@ -160,8 +160,7 @@ vector<int> executeGame(vector<unsigned char> &allAnimatStates, vector<int>
                     for (int n = 0; n < agent->mNumSensors; n++)
                         allAnimatStates[allAnimatStatesIndex++] = agent->states[n];
 
-                    for (int i = 0; i < 3; i++)
-                        agent->updateStates();
+                    agent->updateStates();
 
                     // Record state of hidden units and motors after updating animat
                     for (int n = agent->mNumSensors; n < agent->mNumNodes; n++) {
