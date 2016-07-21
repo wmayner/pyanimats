630ce5675877a4313cc2c6df64ddfeabbae2573e
Will Mayner <wmayner@gmail.com>
2016-07-21 17:18:16 -0500

    Serialize NaN and inf values, pass kwargs

--- pyanimats/serialize.py
+++ pyanimats/serialize.py
@@ -8,22 +8,42 @@ on objects if available.
 """
 
 from collections import Iterable
+import math
+import sys
 
 import numpy as np
 
 
-def serializable(obj, method='serializable'):
+def serializable(obj, method='serializable', nan_to_num=True, **kwargs):
     """Return a serializable representation of an object, recursively calling
-    ``method`` if available and converting NumPy types to native types."""
+    ``method`` if available and converting NumPy types to native types.
+
+    ``inf``, ``-inf``, and ``NaN`` values are converted by default to a very
+    large number, a very small number, and zero, respectively. This can be
+    disabled with the ``nan_to_num`` keyword argument.
+
+    If ``obj`` has ``method``, then further keyword arguments are passed to it
+    (but they are not passed recursively.)
+    """
     # Call the serializable method if available.
     if method is not None and hasattr(obj, method):
-        return serializable(getattr(obj, method)())
+        return serializable(getattr(obj, method)(**kwargs))
     # If we have a NumPy array, convert it to a list.
     if isinstance(obj, np.ndarray):
-        return obj.tolist()
-    # If we have NumPy datatypes, convert them to native types.
+        return np.nan_to_num(obj).tolist()
+    # If we have a NumPy integer, convert it to a native integer.
     if isinstance(obj, np.integer):
         return int(obj)
+    if nan_to_num:
+        # If we have native infinite or NaN values, convert them to numbers.
+        if isinstance(obj, float):
+            if math.isnan(obj):
+                return 0.0
+            if obj == float('inf'):
+                return sys.float_info.max
+            if obj == float('-inf'):
+                return sys.float_info.min
+    # If we have other NumPy types, convert them to their native equivalents.
     if isinstance(obj, (np.float16, np.float32, np.float64)):
         return float(obj)
     if isinstance(obj, np.bool_):
