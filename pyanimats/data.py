70650b004cd7ff7fa41e5ef02e71544b26c23b7e
Will Mayner <wmayner@gmail.com>
2016-08-11 12:11:50 -0500

    Allow loading from gzipped output in `data.py`

--- pyanimats/data.py
+++ pyanimats/data.py
@@ -12,28 +12,30 @@ from glob import glob
 from . import evolve
 
 
-def load(filepath, gzipped=False):
-    if gzipped:
-        _open = gzip.open
+def load(filepath, convert=True, compressed=False, quiet=False):
+    if not quiet:
+        print('Loading {}...'.format(filepath), end="", flush=True)
+
+    _, ext = os.path.splitext(filepath)
+    if compressed or ext in ['.gz', '.gzip']:
+        with gzip.open(filepath, 'rb') as f:
+            d = json.loads(f.read().decode('ascii'))
     else:
-        _open = open
-    with _open(filepath, 'r') as f:
-        return evolve.from_json(json.load(f))
+        with open(filepath, 'rt') as f:
+            d = json.load(f)
 
-
-def load_all(directory, pattern=os.path.join('**', 'output.json'),
-             convert=True):
-    """Recursively load files matching ``pattern``."""
-    data = []
-    paths = glob(os.path.join(directory, pattern))
-    for path in paths:
-        print('Loading {}...'.format(path))
-        try:
-            with open(path, 'r') as f:
-                data.append(json.load(f))
-        except Exception as e:
-            print('  Error: {}'.format(e))
-            print('  Could not load file; skipping...')
     if convert:
-        data = list(map(evolve.from_json, data))
-    return data
+        d = evolve.from_json(d)
+    print('done.')
+    return d
+
+
+def load_all(directory, pattern=os.path.join('output*.json*'), **kwargs):
+    """Recursively load files in ``directory`` matching ``pattern``."""
+    d = []
+    for path in glob(os.path.join(directory, pattern)):
+        try:
+            d.append(load(path, **kwargs))
+        except Exception as e:
+            print('\n  Error: {}'.format(e))
+            print('  Could not load file; skipping...')
