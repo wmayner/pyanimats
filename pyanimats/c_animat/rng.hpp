fd3b6b29ac854f6c383535273c63aeab1f6084a1
Will Mayner <wmayner@gmail.com>
2016-11-13 16:03:19 -0600

    C++ RNG: set `randDouble` limits with doubles

--- pyanimats/c_animat/rng.hpp
+++ pyanimats/c_animat/rng.hpp
@@ -12,7 +12,7 @@
 
 static std::mt19937 mersenne(1729);
 static std::uniform_int_distribution<int> uniform_int_dist(0, RAND_MAX);
-static std::uniform_real_distribution<double> uniform_double_dist(0, 1);
+static std::uniform_real_distribution<double> uniform_double_dist(0.0, 1.0);
 static std::uniform_int_distribution<int> uniform_char_int_dist(0, 255);
 
 double randDouble();
